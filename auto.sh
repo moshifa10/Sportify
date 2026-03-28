4be865e5f94589f64a0b88d8085ef49fca8e1ad6
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-03-29 01:55:22 +0200

    added the requests

---
+++ auto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add main.py requirements auto.sh
+git commit -m "added the requests"
+git push
--- main.py
+++ main.py
@@ -6,4 +6,8 @@ import dotenv
 args = argparse.ArgumentParser("This is a programme to choose a date and select a year and will provide a playlist during that year")
 
 args.add_argument("-d", "--date", required=True, help="Can you do this in this format: YYYY-MM-DD")
+parser = args.parse_args()
 
+url = f"https://www.billboard.com/charts/hot-100/{parser.date}/"
+response = requests.get(url=url)
+print(response.text)
